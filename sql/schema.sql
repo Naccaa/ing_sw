93d37b79a039c3c9e0a25aa68b9fcb956f7b2914
simo4523 <88596251+simo4523@users.noreply.github.com>
2025-12-01 17:03:40 +0100

    aggiunte tabelle Users, Emergencies e Guidelines

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,67 @@
 CREATE SCHEMA ing_sw;
 
+CREATE TYPE ing_sw.user_roles AS ENUM (
+  'cittadino',
+  'admin',
+);
+
+CREATE TYPE ing_sw.user_health AS ENUM (
+  "al sicuro",
+  "in pericolo"
+);
+
+CREATE TYPE ing_sw.user_status AS (
+  health ing_sw.user_health,
+  location point,
+  time timestampz
+);
+
+CREATE TABLE ing_sw.Users (
+  email varchar PRIMARY KEY,
+  caregiver_email varchar DEFAULT NULL,
+
+  role ing_sw.user_roles NOT NULL,
+  fullname varchar NOT NULL,
+  phone_number varchar NOT NULL,
+  last_status ing_sw.user_status DEFAULT NULL,
+
+  password_salt_hex varchar NOT NULL,
+  password_digest_hex varchar NOT NULL
+
+  FOREIGN KEY (caregiver_email) REFERENCES ing_sw.Users (email)
+  ON UPDATE CASCADE
+	ON DELETE SET NULL
+);
+
+CREATE TYPE ing_sw.emergency_type AS ENUM (
+  "allagamento",
+  "alluvione",
+  "grandinata",
+  "tromba d'aria",
+  "alro"
+);
+
+CREATE TABLE ing_sw.Emergencies (
+  id serial PRIMARY KEY,
+  emergency_type ing_sw.emergency_type NOT NULL,
+  message varchar NOT NULL,
+  
+  location point NOT NULL,
+  radius double NOT NULL,
+
+  start_time timestampz NOT NULL,
+  end_time timestampz DEFAULT NULL
+);
+
+CREATE TABLE ing_sw.Guidelines (
+  emergency_type ing_sw.emergency_type PRIMARY KEY,
+  message varchar NOT NULL
+);
+
+
+
+
+
 CREATE TYPE ing_sw.status AS ENUM (
   'fine',
   'pending',
