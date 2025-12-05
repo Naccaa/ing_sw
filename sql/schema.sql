a631dfc46cc50952ab9fbf92624e04c5109c0e45
simo4523 <88596251+simo4523@users.noreply.github.com>
2025-12-05 09:29:21 +0100

    semplificata tabella Users

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,34 +1,26 @@
 CREATE SCHEMA ing_sw;
 
-CREATE TYPE ing_sw.user_roles AS ENUM (
-  'cittadino',
-  'admin',
+CREATE TYPE ing_sw.user_status AS ENUM (
+  'fine',
+  'pending',
+  'inDanger'
 );
 
-CREATE TYPE ing_sw.user_health AS ENUM (
-  'al sicuro',
-  'in pericolo'
-);
-
-CREATE TYPE ing_sw.user_status AS (
-  health ing_sw.user_health,
-  location point,
-  time timestampz
-);
-
-CREATE TABLE ing_sw.Users (
-  email varchar PRIMARY KEY,
-  caregiver_email varchar DEFAULT NULL,
-
-  role ing_sw.user_roles NOT NULL,
-  fullname varchar NOT NULL,
-  phone_number varchar NOT NULL,
-  last_status ing_sw.user_status DEFAULT NULL,
-
+CREATE TABLE taw_bd.Users (
+  userId serial PRIMARY KEY,
+	caregiverId serial DEFAULT NULL,
+  email varchar NOT NULL UNIQUE,
+  fullName varchar NOT NULL,
+	phoneNumber varchar NOT NULL,
+	status ing_sw.user_status NOT NULL DEFAULT 'fine',
+	status_time timestampz DEFAULT NULL,
+	lastLocation point DEFAULT NULL,
+	lastLocationTime timestampz DEFAULT NULL,
+  isAdmin boolean NOT NULL DEFAULT FALSE,
   password_salt_hex varchar NOT NULL,
-  password_digest_hex varchar NOT NULL
+  password_digest_hex varchar NOT NULL,
 
-  FOREIGN KEY (caregiver_email) REFERENCES ing_sw.Users (email)
+  FOREIGN KEY (caregiverId) REFERENCES ing_sw.Users (userId)
   ON UPDATE CASCADE
 	ON DELETE SET NULL
 );
@@ -38,50 +30,24 @@ CREATE TYPE ing_sw.emergency_type AS ENUM (
   'alluvione',
   'grandinata',
   'tromba d''aria',
-  'alro'
+  'altro'
 );
 
 CREATE TABLE ing_sw.Emergencies (
   id serial PRIMARY KEY,
-  emergency_type ing_sw.emergency_type NOT NULL,
+  emergency_type ing_sw.emergency_type NOT NULL DEFAULT 'altro',
   message varchar NOT NULL,
   
   location point NOT NULL,
   radius double NOT NULL,
 
   start_time timestampz NOT NULL,
-  end_time timestampz DEFAULT NULL
+  end_time timestampz DEFAULT NULL,
+
+  CHECK(end_time IS NULL OR start_time < end_time)
 );
 
 CREATE TABLE ing_sw.Guidelines (
   emergency_type ing_sw.emergency_type PRIMARY KEY,
   message varchar NOT NULL
 );
-
-
-
-
-/* altra proposta semplificata per gli user
-CREATE TYPE ing_sw.status AS ENUM (
-  'fine',
-  'pending',
-  'inDanger'
-);
-
-CREATE TABLE taw_bd.Users (
-  userId serial PRIMARY KEY,
-  email varchar NOT NULL UNIQUE,
-  fullName varchar NOT NULL,
-	phoneNumber varchar NOT NULL,
-	status ing_sw.status NOT NULL DEFAULT 'fine',
-	caregiver serial DEFAULT NULL,
-	-- lastLocation ??
-  isAdmin boolean NOT NULL DEFAULT FALSE,
-  salt varchar NOT NULL,
-  hash varchar NOT NULL,
-
-  FOREIGN KEY (caregiver) REFERENCES ing_sw.Users (userId)
-  ON UPDATE CASCADE
-	ON DELETE SET NULL
-);
-*/
