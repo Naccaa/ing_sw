bbd2831820f2f9633cfbf6fc411725703a8e2791
Alessandro Lacava <alessandrolacava47@gmail.com>
2025-12-16 15:58:36 +0100

    Working on login and session

---
+++ android/app/src/main/java/com/example/ids/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.example.ids.constants;
+
+public class Constants {
+    // Indirizzo usato dalle chiamate al backend
+    public static final String BASE_URL = "http://192.168.1.50:5000";
+}
