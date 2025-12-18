7a3c0588e68eef7d66ddc067f133484f813bb82b
Alessandro Lacava <alessandrolacava47@gmail.com>
2025-12-18 22:52:16 +0100

    Removed back arrow from login and password reset interfaces

--- android/app/src/main/java/com/example/ids/MainActivity.java
+++ android/app/src/main/java/com/example/ids/MainActivity.java
@@ -28,8 +28,12 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_alert, R.id.navigation_guide, R.id.navigation_settings)
-                .build();
+                R.id.navigation_alert,
+                R.id.navigation_guide,
+                R.id.navigation_settings,
+                R.id.navigation_login,
+                R.id.navigation_forgotPassword
+                ).build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
