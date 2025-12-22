292f8a97b3005f7966d7309cabc4a5b0ca32e0d0
simo4523 <88596251+simo4523@users.noreply.github.com>
2025-12-22 21:56:14 +0100

    fix build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,20 +45,13 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-<<<<<<< HEAD
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.8.8")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-=======
     implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    // firebase
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
->>>>>>> notifications
 }
