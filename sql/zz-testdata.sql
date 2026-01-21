e88f3b342c44985e45c13340f600b03b31cfcb11
simo4523 <88596251+simo4523@users.noreply.github.com>
2026-01-21 13:25:45 +0100

    add: admin account

---
+++ sql/zz-testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO ing_sw.Users (email, fullname, phone_number, is_admin, password_salt_hex, password_digest_hex) VALUES
+('admin', 'admin admin', '1', TRUE, 'd28a3390f370e1732851012e2832faca', '55f70d0e16369bae7aa74e212af5a664f47626e606dadcc8565c83e714e89ea1e4772b42584493b5dd47968b64d0fd0c823f4114fdbef81cc9657c46bc53997a');
